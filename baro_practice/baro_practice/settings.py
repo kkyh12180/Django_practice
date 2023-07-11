f62037948d569b55559295a6ffccac0691f74083
Hyo <kyh12180@gmail.com>
2023-07-11 16:03:16 +0900

    Setting.py edit

--- baro_practice/baro_practice/settings.py
+++ baro_practice/baro_practice/settings.py
@@ -10,20 +10,18 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
 import environ
 import os
+from pathlib import Path
 
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Take environment variables from .env file
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +35,7 @@ DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -47,7 +46,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accountapp',
-
 ]
 
 MIDDLEWARE = [
