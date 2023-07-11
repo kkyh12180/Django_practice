23e7008464ea4e438bb7eabde72abeb0e62bd736
Hyo <kyh12180@gmail.com>
2023-07-11 15:46:01 +0900

    First commit

---
+++ baro_practice/accountapp/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from accountapp.views import hello_world
+
+# app_name 명시
+# 추후 accountapp:hello_world 로 접근이 가능해지기 때문
+app_name = 'accountapp'
+
+urlpatterns = [
+    # views.py의 hello_world 불러옴
+    path('hello_world/', hello_world, name='hello_world'),
+]
