14adf61ce5a435cea730b6f2e59be8b6f6516203
Hyo <kyh12180@gmail.com>
2023-07-12 17:02:04 +0900

    Lesson 17

--- baro_practice/account_app/views.py
+++ baro_practice/account_app/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from account_app.models import HelloWorld
 
 # Create your views here.
 
 def hello_world(request) :
 
     if request.method == 'POST' :
-        return render(request, 'account_app/hello_world.html', context={'text': 'POST METHOD!'})
+        # post 에서 데이터를 가져옴
+        temp = request.POST.get('hello_world_input')   
+        
+        # model DB 내부에 저장
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        return render(request, 'account_app/hello_world.html', context={'hello_world_output': new_hello_world})
     return render(request, 'account_app/hello_world.html', context={'text': 'GET METHOD!'})
