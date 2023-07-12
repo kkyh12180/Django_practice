2be85fd6d119595ddf2d75ac178f2a7aca95af9f
Hyo <kyh12180@gmail.com>
2023-07-12 16:17:59 +0900

    Lesson 14

--- baro_practice/baro_practice/settings.py
+++ baro_practice/baro_practice/settings.py
@@ -128,6 +128,10 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static" ,
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
